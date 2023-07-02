3bf580e857300af7e6d83f2b5466be6d1ac425e6
Hama <hamashi@88665a2e9fc9.ant.amazon.com>
2023-07-03 02:00:51 +0900

    composer downloaded

--- build.sh
+++ build.sh
@@ -6,12 +6,31 @@ PYTHON=python2 amazon-linux-extras install epel
 
 yum -y install supervisor
 
-composer --version
 yum install php-mbstring php-xml -y
+
+# Composer install
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+echo "export PATH=~/.config/composer/vendor/bin:$PATH" >> ~/.bash_profile
+source ~/.bash_profile
+
+# dependencies install
 composer install
 cp -p .env.example .env
 php artisan key:generate
+
+# Permission
+find ./storage -type d -exec chmod 775 {} \;
+find ./storage -type f -exec chmod 664 {} \;
+
+find ./bootstrap/cache -type d -exec chmod 775 {} \;
+find ./bootstrap/cache -type f -exec chmod 664 {} \;
+
+find ./storage -type d -exec chmod g+s {} \;
+find ./bootstrap/cache -type d -exec chmod g+s {} \;
+
+setfacl -R -d -m g::rwx ./storage
+setfacl -R -d -m g::rwx ./bootstrap/cache
